45653d54561242be88d3484b19cb3f1efaa9dd24
Niyizigihe <niyizigihe16@gmail.com>
2023-06-22 20:55:56 +0200

    added while loopprint("sum is ${calcSum(2, 2)}");

--- loops.dart
+++ loops.dart
@@ -1,5 +1,15 @@
+// main() {
+//   for (int i = 1; i <= 10; i++) {
+//     print("Evar");
+//   }
+// }
+
 main() {
-  for (int i = 1; i <= 10; i++) {
-    print("Evar");
+  int n = 1;
+  int sum = 0;
+  while (n <= 10) {
+    sum += n;
+    n++;
   }
+  print("The sum is $sum");
 }
