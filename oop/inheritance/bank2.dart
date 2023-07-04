54dce9a45e1ca30c7c75492c695fc5faa6671943
Niyizigihe <niyizigihe16@gmail.com>
2023-07-04 16:52:04 +0200

    mmmmm

---
+++ oop/inheritance/bank2.dart
@@ -0,0 +1,42 @@
+class Branches
+{
+    String? b_name;
+    set bname(String name)=> b_name=name;
+}
+class BankAccount extends Branches
+{
+    late int _acc_no;
+    double? _balance;
+    int get accno=>this._acc_no;
+    double get bal=>this._balance!;
+   setDeposit(double amount)
+   {
+    _balance =_balance!+amount;
+    print("you deposited $amount from $bname branch. new balance is $_balance");
+   }
+   setWithdraw(double amount)
+   {
+    if(amount>_balance!)
+    {
+      print("Not enough funds.");
+    }
+    else{
+
+    
+    _balance =_balance!-amount;
+    print("you have withdrawn $amount from $bname branch. new balance is $_balance");
+    }
+   }
+}
+
+
+main()
+{
+  var bank=BankAccount();
+  bank.bname="BK Karongi";
+  bank._balance=30;
+
+  print("Balance: ${bank.bal}");
+  bank.setDeposit(500);
+  bank.setWithdraw(12);
+}
